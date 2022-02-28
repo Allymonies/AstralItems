0d72b3535c7086dcb1d0ce7c4ccc6d5d25322b1f
HolyAntimony <maya@alyssa.ws>
2022-02-27 21:41:57 -0500

    Organize imports

--- src/main/java/io/astralforge/astralitems/recipe/AstralRecipeChoice.java
+++ src/main/java/io/astralforge/astralitems/recipe/AstralRecipeChoice.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 
 import com.google.common.collect.Lists;
 
--- src/main/java/io/astralforge/astralitems/recipe/AstralRecipeEvaluator.java
+++ src/main/java/io/astralforge/astralitems/recipe/AstralRecipeEvaluator.java
@@ -13,8 +13,6 @@ import org.bukkit.Keyed;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
-import org.bukkit.inventory.RecipeChoice;
-import org.bukkit.inventory.ShapedRecipe;
 
 import lombok.Getter;
 

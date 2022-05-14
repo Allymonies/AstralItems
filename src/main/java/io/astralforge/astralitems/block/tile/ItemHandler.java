267d6eb72a8435e10dec3597c2968ea5db244f59
HolyAntimony <maya@alyssa.ws>
2022-05-13 17:54:13 -0700

    Make item handler methods nullable

--- src/main/java/io/astralforge/astralitems/block/tile/ItemHandler.java
+++ src/main/java/io/astralforge/astralitems/block/tile/ItemHandler.java
@@ -2,15 +2,24 @@ package io.astralforge.astralitems.block.tile;
 
 import org.bukkit.inventory.ItemStack;
 
+import javax.annotation.Nullable;
+
 public interface ItemHandler {
     // Inspired by Forge IItemHandler
     int getSize();
+    @Nullable
     ItemStack getItem(int slot);
     void setItem(int slot, ItemStack stack);
+    @Nullable
     ItemStack extractItem(int slot, int amount, boolean simulate);
+    @Nullable
     ItemStack extractItem(int slot, int amount);
+    @Nullable
     ItemStack extractItem(int amount);
+    @Nullable
     ItemStack insertItem(int slot, ItemStack item);
+    @Nullable
     ItemStack insertItem(int slot, ItemStack item, boolean simulate);
+    @Nullable
     ItemStack insertItem(ItemStack item);
 }
